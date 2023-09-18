d552be32e81f7f39562a22a6f8749d1b00ddd353
Elisol Bastouri <elisol.bastouri@fanduel.com>
2023-09-18 16:17:03 +0100

    Implement logic for the user's total

--- src/components/ShoppingBasket.tsx
+++ src/components/ShoppingBasket.tsx
@@ -7,6 +7,8 @@ import {
 } from "react-bootstrap";
 import { useShoppingBasket } from "../context/BasketContext";
 import { BasketItem } from "./BasketItem";
+import { Currency } from "../utilities/Currency";
+import products from "../data/products.json";
 
 type ShoppingBasketProps = {
   isOpen: boolean;
@@ -14,7 +16,6 @@ type ShoppingBasketProps = {
 
 export function ShoppingBasket({ isOpen }: ShoppingBasketProps) {
   const { closeBasket, basketItems } = useShoppingBasket();
-  //implementing offcanvas from bootstrap
   return (
     <Offcanvas show={isOpen} onHide={closeBasket} placement="end">
       <OffcanvasHeader closeButton>
@@ -25,6 +26,15 @@ export function ShoppingBasket({ isOpen }: ShoppingBasketProps) {
           {basketItems.map((item) => (
             <BasketItem key={item.id} {...item} />
           ))}
+          <div className="ms-auto fw-bold fs-5">
+            Your Total is:{" "}
+            {Currency(
+              basketItems.reduce((total, basketItem) => {
+                const item = products.find((i) => i.id === basketItem.id);
+                return total + (item?.price || 0) * basketItem.quantity;
+              }, 0)
+            )}
+          </div>
         </Stack>
       </OffcanvasBody>
     </Offcanvas>
